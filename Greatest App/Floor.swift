f18a80dc191ba5427d3a41c0c75c8a602ecc1802
Vibha Alangar <alangavr@rose-hulman.edu>
2018-04-17 21:01:43 -0400

    added Floor model object

---
+++ Greatest App/Floor.swift
@@ -0,0 +1,20 @@
+//
+//  Floor.swift
+//  Greatest App
+//
+//  Created by CSSE Department on 4/17/18.
+//  Copyright © 2018 CSSE Department. All rights reserved.
+//
+
+import UIKit
+
+class Floor: NSObject {
+    
+    var name: String
+    var residents = [User]()
+    var participation = [Int]()
+    
+    init(name: String){
+        self.name = name;
+    }
+}
