ed51a6dff6d544648acf1602b7a9d2ea6de7c786
Kiana Caston <castonkr@rose-hulman.edu>
2018-04-25 15:29:07 -0400

    added to GFEvent object

--- Greatest App/GFEvent.swift
+++ Greatest App/GFEvent.swift
@@ -7,20 +7,48 @@
 //
 
 import UIKit
+import Firebase
 
 class GFEvent: NSObject {
     
+    var id: String?
     var name: String
     var time: String
     var location: String
     var minParticipants: Int?
     var maxParticipants: Int?
-    var eventDescription: String?
+    var eventDescription: String
     
-    init(name: String, time: String, location:String) {
+    let nameKey = "name"
+    let timeKey = "time"
+    let locationKey = "location"
+    let minParticipantsKey = "minParticipantsKey"
+    let maxParticipantsKey = "maxParticipantsKey"
+    let eventDescriptionKey = "eventDescription"
+    
+    
+    init(name: String, time: String, location:String, eventDescription: String) {
         self.name = name
         self.time = time
         self.location = location
+        self.eventDescription = eventDescription
     }
-
+    
+    init(documentSnapshot: DocumentSnapshot) {
+        self.id = documentSnapshot.documentID
+        let data = documentSnapshot.data()!
+        self.name = data[nameKey] as! String
+        self.time = data[timeKey] as! String
+        self.location = data[locationKey] as! String
+        self.eventDescription = data[eventDescriptionKey] as! String
+        
+    }
+    
+    var data: [String: Any] {
+        return [nameKey: self.name,
+                timeKey: self.time,
+                locationKey: self.location,
+                eventDescriptionKey: self.eventDescription]
+    }
+    
 }
